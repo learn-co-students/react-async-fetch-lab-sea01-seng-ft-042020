0e84c14fc04976f9eb95aae35cb72231c5c6acb8
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-06-26 11:15:16 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
-// create your App component here
+import React, { Component } from 'react';
+import { render } from 'enzyme';
+
+class App extends Component {
+
+    state = {data: []}
+
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json").then(resp => resp.json())
+        .then(json => this.setState({data: json}))
+    }
+
+    render() {
+        return (this.state.data)
+    }
+}
+
+
+
+export default App;
